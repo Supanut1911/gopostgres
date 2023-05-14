5da11fffee2b419e3abf937c30d4b03eaabe8768
Supanut1911 <ynotmela@gmail.com>
2023-05-14 23:53:13 +0700

    deal with query

--- main.go
+++ main.go
@@ -8,6 +8,33 @@ import (
 )
 
 func main(){
-	var dataSoruce = fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=%s", "localhost", 5432, "postgres", "postgres", "godb", "disable")
-	sql.Open("postgres",dataSoruce)
+	var dataSoruce = fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=%s", "localhost", 5432, "postgres", "postgres", "dbpg", "disable")
+	db, err := sql.Open("postgres",dataSoruce)
+	if err != nil {
+		panic(err)
+	}
+
+	//check db is already connect
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	//query
+	query := "select * from cover"
+	rows, err :=  db.Query(query)
+	if err != nil {
+		panic(err)
+	}
+
+	id := 0
+	name := ""
+	ok := rows.Next()
+	if ok {
+		rows.Scan(&id ,&name)
+	}
+
+	rows.Close()
+
+	println(id, name)
 }
